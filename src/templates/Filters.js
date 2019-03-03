6e10e65b5c14b554d66e202af62b5840162a9e80
AnorVen <efremovgs@gmail.com>
2019-03-03 13:09:03 +0300

    random points

--- src/templates/Filters.js
+++ src/templates/Filters.js
@@ -1,5 +1,7 @@
-const filterRender = ({TITLE, CHECKED = false})=>(`
-  <input type="radio" id="filter-${TITLE.toLowerCase()}" name="filter" value="${TITLE.toLowerCase()}" ${CHECKED ? `checked` : null}>
+const filterRender = ({TITLE, CHECKED = false}) => `
+  <input type="radio" id="filter-${TITLE.toLowerCase()}" name="filter" value="${TITLE.toLowerCase()}" ${
+  CHECKED ? `checked` : null
+}>
           <label class="trip-filter__item" for="filter-${TITLE.toLowerCase()}">${TITLE}</label>
-`);
+`;
 export default filterRender;
