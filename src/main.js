e1fb679ff88e6c1f82b035e95dee8b1d8e35a5b1
AnorVen <efremovgs@gmail.com>
2019-03-09 19:53:37 +0300

    module3 task2

--- src/main.js
+++ src/main.js
@@ -2,18 +2,31 @@ import tripPointRender from './templates/TripPoints';
 import filterRender from './templates/Filters';
 
 const POINT_VARIABLES = {
-  icon: {
-    taxi: `🚕`,
-    air: `✈`,
-    car: `🚗`,
-    hotel: `🏨`,
-  },
-  title: Math.floor(Math.random() * 201),
-  timetable: Math.floor(Math.random() * 201),
-  duration: Math.floor(Math.random() * 201),
-  price: Math.floor(Math.random() * 201),
-  offers: [Math.floor(Math.random() * 201)],
-};
+    icon: {
+      Taxi: `🚕`,
+      Bus: `🚌`,
+      Train: `🚂`,
+      Ship: `🛳️`,
+      Transport: `🚊`,
+      Drive: `🚗`,
+      Flight: `✈`,
+      CheckIn: `🏨`,
+      Sightseeing: `🏛️`,
+      Restaurant: `🍴`
+    },
+    title:[`Taxi to Airport`, `Taxi to Airport`,`Drive to Chamonix`,`Check into a hotel`],
+    timetable: Date.now() + 1 + Math.floor(Math.random() * 7) * (Math.floor(Math.random() * 24)) * (Math.floor(Math.random() * 60)) * 60 * 1000,
+    duration: ``,
+    price:
+     '',
+    offers: [`Add luggage`, `Switch to comfort class`, `Add meal`, `Choose seats`],
+
+  destination: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras aliquet varius magna, non porta ligula feugiat eget. Fusce tristique felis at fermentum pharetra. Aliquam id orci ut lectus varius viverra. Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante. Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum. Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui. Sed sed nisi sed augue convallis suscipit in sed felis. Aliquam erat volutpat. Nunc fermentum tortor ac porta dapibus. In rutrum ac purus sit amet tempus.`.split(`. `),
+  picture: `http://picsum.photos/100/100?r=${Math.random()}`,
+  isEdit: false,
+
+  }
+;
 const DB = {
   FILTERS_DATA: [
     {
@@ -31,65 +44,73 @@ const DB = {
   ],
   POINTS_DATA: [
     {
-      icon: POINT_VARIABLES.icon.taxi,
-      title: `Taxi to Airport`,
-      timetable: `10:00&nbsp;&mdash; 11:00`,
-      duration: `1h 30m`,
-      price: `&euro;&nbsp;20`,
-      offers: [`Order UBER +&euro;&nbsp;20`, `Upgrade to business +&euro;&nbsp;20`],
+      icon: POINT_VARIABLES.icon[Object.keys(POINT_VARIABLES.icon)
+        [Math.floor(Math.random() * Object.keys(POINT_VARIABLES.icon).length)]],
+      title: POINT_VARIABLES.title[Math.floor(Math.random() * POINT_VARIABLES.title.length)],
+      timetable: POINT_VARIABLES.timetable,
+      duration: Math.floor(Math.random() * 60 * 60 * 24 * 1000),
+      price: `${ Math.floor(Math.random() * 201)}`,
+      offers: POINT_VARIABLES.offers,
     },
     {
-      icon: POINT_VARIABLES.icon.taxi,
-      title: `Taxi to Airport`,
-      timetable: `10:00&nbsp;&mdash; 11:00`,
-      duration: `1h 30m`,
-      price: `&euro;&nbsp;20`,
-      offers: [`Order UBER +&euro;&nbsp;20`, `Upgrade to business +&euro;&nbsp;20`],
+      icon: POINT_VARIABLES.icon[Object.keys(POINT_VARIABLES.icon)
+        [Math.floor(Math.random() * Object.keys(POINT_VARIABLES.icon).length)]],
+      title: POINT_VARIABLES.title[Math.floor(Math.random() * POINT_VARIABLES.title.length)],
+      timetable: POINT_VARIABLES.timetable,
+      duration: Math.floor(Math.random() * 60 * 60 * 24 * 1000),
+      price: `${ Math.floor(Math.random() * 201)}`,
+      offers: POINT_VARIABLES.offers,
     },
     {
-      icon: POINT_VARIABLES.icon.taxi,
-      title: `Taxi to Airport`,
-      timetable: `10:00&nbsp;&mdash; 11:00`,
-      duration: `1h 30m`,
-      price: `&euro;&nbsp;20`,
-      offers: [`Order UBER +&euro;&nbsp;20`, `Upgrade to business +&euro;&nbsp;20`],
+      icon: POINT_VARIABLES.icon[Object.keys(POINT_VARIABLES.icon)
+        [Math.floor(Math.random() * Object.keys(POINT_VARIABLES.icon).length)]],
+      title: POINT_VARIABLES.title[Math.floor(Math.random() * POINT_VARIABLES.title.length)],
+      timetable: POINT_VARIABLES.timetable,
+      duration: Math.floor(Math.random() * 60 * 60 * 24 * 1000),
+      price: `${ Math.floor(Math.random() * 201)}`,
+      offers: POINT_VARIABLES.offers,
     },
     {
-      icon: POINT_VARIABLES.icon.taxi,
-      title: `Taxi to Airport`,
-      timetable: `10:00&nbsp;&mdash; 11:00`,
-      duration: `1h 30m`,
-      price: `&euro;&nbsp;20`,
-      offers: [`Order UBER +&euro;&nbsp;20`, `Upgrade to business +&euro;&nbsp;20`],
+      icon: POINT_VARIABLES.icon[Object.keys(POINT_VARIABLES.icon)
+        [Math.floor(Math.random() * Object.keys(POINT_VARIABLES.icon).length)]],
+      title: POINT_VARIABLES.title[Math.floor(Math.random() * POINT_VARIABLES.title.length)],
+      timetable: POINT_VARIABLES.timetable,
+      duration: Math.floor(Math.random() * 60 * 60 * 24 * 1000),
+      price: `${ Math.floor(Math.random() * 201)}`,
+      offers: POINT_VARIABLES.offers,
     },
     {
-      icon: POINT_VARIABLES.icon.taxi,
-      title: `Taxi to Airport`,
-      timetable: `10:00&nbsp;&mdash; 11:00`,
-      duration: `1h 30m`,
-      price: `&euro;&nbsp;20`,
-      offers: [`Order UBER +&euro;&nbsp;20`, `Upgrade to business +&euro;&nbsp;20`],
+      icon: POINT_VARIABLES.icon[Object.keys(POINT_VARIABLES.icon)
+        [Math.floor(Math.random() * Object.keys(POINT_VARIABLES.icon).length)]],
+      title: POINT_VARIABLES.title[Math.floor(Math.random() * POINT_VARIABLES.title.length)],
+      timetable: POINT_VARIABLES.timetable,
+      duration: Math.floor(Math.random() * 60 * 60 * 24 * 1000),
+      price: `${ Math.floor(Math.random() * 201)}`,
+      offers: POINT_VARIABLES.offers,
     },
     {
-      icon: POINT_VARIABLES.icon.taxi,
-      title: `Taxi to Airport`,
-      timetable: `10:00&nbsp;&mdash; 11:00`,
-      duration: `1h 30m`,
-      price: `&euro;&nbsp;20`,
-      offers: [`Order UBER +&euro;&nbsp;20`, `Upgrade to business +&euro;&nbsp;20`],
+      icon: POINT_VARIABLES.icon[Object.keys(POINT_VARIABLES.icon)
+        [Math.floor(Math.random() * Object.keys(POINT_VARIABLES.icon).length)]],
+      title: POINT_VARIABLES.title[Math.floor(Math.random() * POINT_VARIABLES.title.length)],
+      timetable: POINT_VARIABLES.timetable,
+      duration: Math.floor(Math.random() * 60 * 60 * 24 * 1000),
+      price: `${ Math.floor(Math.random() * 201)}`,
+      offers: POINT_VARIABLES.offers,
     },
     {
-      icon: POINT_VARIABLES.icon.taxi,
-      title: `Taxi to Airport`,
-      timetable: `10:00&nbsp;&mdash; 11:00`,
-      duration: `1h 30m`,
-      price: `&euro;&nbsp;20`,
-      offers: [`Order UBER +&euro;&nbsp;20`, `Upgrade to business +&euro;&nbsp;20`],
+      icon: POINT_VARIABLES.icon[Object.keys(POINT_VARIABLES.icon)
+        [Math.floor(Math.random() * Object.keys(POINT_VARIABLES.icon).length)]],
+      title: POINT_VARIABLES.title[Math.floor(Math.random() * POINT_VARIABLES.title.length)],
+      timetable: POINT_VARIABLES.timetable,
+      duration: Math.floor(Math.random() * 60 * 60 * 24 * 1000),
+      price: `${ Math.floor(Math.random() * 201)}`,
+      offers: POINT_VARIABLES.offers,
     },
   ],
 };
 const MainFilter = document.querySelector(`.trip-filter`);
 const TripDayItems = document.querySelector(`.trip-day__items`);
+
 function filtersRender(arr) {
   let tempBlock = ``;
   for (let i = 0; i < arr.length; i++) {
@@ -98,6 +119,7 @@ function filtersRender(arr) {
   MainFilter.insertAdjacentHTML(`beforeend`, tempBlock);
   MainFilter.addEventListener(`click`, clickOnFilterHandler);
 }
+
 function tasksRender(arr) {
   let tempBlock = ``;
   for (let i = 0; i < arr.length; i++) {
@@ -105,13 +127,14 @@ function tasksRender(arr) {
   }
   TripDayItems.insertAdjacentHTML(`beforeend`, tempBlock);
 }
+
 function randomPoint({icon, title, ...rest}) {
   TripDayItems.innerHTML = ``;
   let tempBlock = ``;
   for (let i = 0; i < Math.floor(Math.random() * 20); i++) {
     tempBlock += tripPointRender({
       icon: icon[Object.keys(icon)[Math.floor(Math.random() * Object.keys(icon).length)]],
-      title: title,
+      title: POINT_VARIABLES.title[Math.floor(Math.random() * POINT_VARIABLES.title.length)],
       timetable: Math.floor(Math.random() * 201),
       duration: Math.floor(Math.random() * 201),
       price: Math.floor(Math.random() * 201),
@@ -120,6 +143,7 @@ function randomPoint({icon, title, ...rest}) {
   }
   TripDayItems.insertAdjacentHTML(`beforeend`, tempBlock);
 }
+
 function clickOnFilterHandler(event) {
   let target = event.target;
   while (target !== MainFilter) {
@@ -130,6 +154,7 @@ function clickOnFilterHandler(event) {
     target = target.parentNode;
   }
 }
+
 window.onload = function () {
   filtersRender(DB.FILTERS_DATA);
   tasksRender(DB.POINTS_DATA);
--- src/templates/TripPoints.js
+++ src/templates/TripPoints.js
@@ -1,6 +1,7 @@
 function offerRender(arr) {
+  let newArr = arr.length > 2 ? arr.slice(0, 2) : arr;
   let offters = `<ul class="trip-point__offers">`;
-  for (let offer of arr) {
+  for (let offer of newArr) {
     offters += `<li><button class="trip-point__offer">`;
     offters += offer;
     offters += `</button></li>`;
@@ -9,16 +10,35 @@ function offerRender(arr) {
   return offters;
 }
 
-const tripPointRender = ({icon, title, timetable, duration, price, offers}) => `
+function timeSectionRender(timetable, duration) {
+  let timeStartHours = new Date(timetable).getHours();
+  let timeStartMinutes = new Date(timetable).getMinutes();
+  let timeEndHours = new Date(timetable + duration).getHours();
+  let timeEndMinutes = new Date(timetable + duration).getMinutes();
+  let durationHours = new Date(duration).getHours();
+  let durationMinutes = new Date(duration).getMinutes();
+  durationMinutes.toString().length === 1 ? durationMinutes = `0${durationMinutes}` : null;
+  timeStartMinutes.toString().length === 1 ? timeStartMinutes = `0${timeStartMinutes}` : null;
+  timeEndMinutes.toString().length === 1 ? timeEndMinutes = `0${timeEndMinutes}` : null;
+  /* Даты складываются как-то коряво.. непойму почему
+   console.log(new Date(Date.now() + duration).getHours());
+    console.log(new Date(duration).getHours());
+    console.log(new Date(Date.now()).getHours());*/
+  return `<span class="trip-point__timetable">${timeStartHours}:${timeStartMinutes}&nbsp;&mdash; ${timeEndHours}:${timeEndMinutes}</span>
+            <span class="trip-point__duration">${durationHours}h ${durationMinutes}m</span>`;
+}
+
+const tripPointRender = ({icon, title, timetable, duration, price, offers}) => {
+  return `
  <article class="trip-point">
           <i class="trip-icon">${icon}</i>
           <h3 class="trip-point__title">${title}</h3>
           <p class="trip-point__schedule">
-            <span class="trip-point__timetable">${timetable}</span>
-            <span class="trip-point__duration">${duration}m</span>
+            ${timeSectionRender(timetable, duration)}
           </p>
-          <p class="trip-point__price">${price}</p>
-         ${offerRender(offers)}
+          <p class="trip-point__price">&euro;&nbsp; ${price}</p>
+           ${offerRender(offers)}
         </article>
 `;
+};
 export default tripPointRender;
