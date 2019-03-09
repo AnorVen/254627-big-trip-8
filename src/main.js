092fd20f05ef19a030f040f57372f6cee6e11fd3
AnorVen <efremovgs@gmail.com>
2019-03-09 20:09:54 +0300

    module3 task2

--- src/main.js
+++ src/main.js
@@ -14,12 +14,11 @@ const POINT_VARIABLES = {
       Sightseeing: `🏛️`,
       Restaurant: `🍴`
     },
-    title:[`Taxi to Airport`, `Taxi to Airport`,`Drive to Chamonix`,`Check into a hotel`],
+    title:new Set([`Taxi to Airport`, `Taxi to Airport`,`Drive to Chamonix`,`Check into a hotel`]),
     timetable: Date.now() + 1 + Math.floor(Math.random() * 7) * (Math.floor(Math.random() * 24)) * (Math.floor(Math.random() * 60)) * 60 * 1000,
-    duration: ``,
-    price:
-     '',
-    offers: [`Add luggage`, `Switch to comfort class`, `Add meal`, `Choose seats`],
+    duration: new Date((Math.floor(Math.random() * 60)) * 60 * 1000),
+    price: Math.floor(Math.random() * 201),
+    offers: new Set([`Add luggage`, `Switch to comfort class`, `Add meal`, `Choose seats`]),
 
   destination: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras aliquet varius magna, non porta ligula feugiat eget. Fusce tristique felis at fermentum pharetra. Aliquam id orci ut lectus varius viverra. Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante. Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum. Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui. Sed sed nisi sed augue convallis suscipit in sed felis. Aliquam erat volutpat. Nunc fermentum tortor ac porta dapibus. In rutrum ac purus sit amet tempus.`.split(`. `),
   picture: `http://picsum.photos/100/100?r=${Math.random()}`,
